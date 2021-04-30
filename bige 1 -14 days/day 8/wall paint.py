cd57da23f3b664d9d587e6ac7d7b9cb0f93dbe3a
rayhan-rimon <rayhanrimon17@gmail.com>
2021-04-30 02:43:58 -0700

    intial commit

---
+++ bige 1 -14 days/day 8/wall paint.py
@@ -0,0 +1,17 @@
+# Wall painting exercise
+# How many cage neet to a wall pain
+# Each cage abale to pain 5 square wall
+
+# start programming
+import math
+def paint_cal(height, width, cover):
+    area = height * width
+    num_can = math.ceil(area / cover)
+    print("You will need {a} can of paints".format(a=num_can))
+
+
+
+t_height = int(input("Enter your wall height: "))
+t_width = int(input("Enter your wall width: "))
+coverage = 5
+paint_cal(height=t_height, width=t_width, cover=coverage)
